65502d08d4b97adf709587e7db15d977a5db9a0a
David Prause <praused@hotmail.com>
2017-09-07 11:49:10 -0400

    Example complete

--- Ch12Ex03_SortAndSearchGenericLists/Program.cs
+++ Ch12Ex03_SortAndSearchGenericLists/Program.cs
@@ -10,6 +10,31 @@ namespace Ch12Ex03_SortAndSearchGenericLists
     {
         static void Main(string[] args)
         {
+            Vectors route = new Vectors()
+            {
+                new Vector(2.0, 90.0),
+                new Vector(1.0, 180.0),
+                new Vector(0.5, 45.0),
+                new Vector(2.5, 315.0),
+            };
+
+            Console.WriteLine(route.Sum());
+
+            Comparison<Vector> sorter = new Comparison<Vector>(VectorDelegates.Compare);
+            route.Sort(sorter);
+            ////could simplify code by using a method group instead of lines 23 and 24...
+            //route.Sort(VectorDelegates.Compare);
+            Console.WriteLine(route.Sum());
+
+            Predicate<Vector> searcher = new Predicate<Vector>(VectorDelegates.TopRightQuadrant);
+            //need to initialize an new Vectors collection using IEnumerable<Vector>.
+            //using route.FindAll(searcher) would return a List<Vector> instance instead.
+            Vectors topRightQuadrantRoute = new Vectors(route.FindAll(searcher));
+            ////could simplify code by using a method group instead of lines 29 and 32...
+            //Vectors topRightQuadrantRoute = new Vectors(route.FindAll(VectorDelegates.TopRightQuadrant));
+            Console.WriteLine(topRightQuadrantRoute.Sum());
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Ch12Ex03_SortAndSearchGenericLists/Vector.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch12Ex03_SortAndSearchGenericLists
+{
+    public class Vector
+    {
+        public double? R = null;
+        public double? Theta = null;
+
+        public double? ThetaRadians
+        {
+            get { return (Theta * Math.PI / 180); } //convert degrees to radians
+        }
+
+        public Vector(double? r, double? theta)
+        {
+            //normalize
+            if (r < 0)
+            {
+                r = -r;
+                theta += 180;
+            }
+            theta = theta % 360;
+
+            //assign fields
+            R = r;
+            Theta = theta;
+        }
+
+        public static Vector operator +(Vector op1, Vector op2)
+        {
+            try
+            {
+                //get (x, y) coordinates for new vector
+                double newX = op1.R.Value * Math.Sin(op1.ThetaRadians.Value) + op2.R.Value * Math.Sin(op2.ThetaRadians.Value);
+                double newY = op1.R.Value * Math.Cos(op1.ThetaRadians.Value) + op2.R.Value * Math.Cos(op2.ThetaRadians.Value);
+
+                //convert to (r, theta)
+                double newR = Math.Sqrt(newX * newX + newY * newY);
+                double newTheta = Math.Atan2(newX, newY) * 180.0 / Math.PI;
+
+                //return result
+                return new Vector(newR, newTheta);
+            }
+            catch
+            {
+                return new Vector(null, null); //return null vector
+            }
+        }
+
+        public static Vector operator-(Vector op1)
+        {
+            return new Vector(-op1.R, op1.Theta);
+        }
+
+        public static Vector operator-(Vector op1, Vector op2)
+        {
+            return op1 + (-op2);
+        }
+
+        public override string ToString()
+        {
+            //get string representation of coordinates
+            string rString = R.HasValue ? R.ToString() : "null";
+            string thetaString = Theta.HasValue ? Theta.ToString() : "null";
+            //return (r, theta) string
+            return string.Format($"({rString}, {thetaString})");
+        }
+    }
+}
