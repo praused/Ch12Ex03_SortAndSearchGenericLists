8c776caed380bb9159595ea04d0d871d2bfc3a1b
David Prause <praused@hotmail.com>
2017-09-07 10:50:25 -0400

    Add project files.

---
+++ Ch12Ex03_SortAndSearchGenericLists/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch12Ex03_SortAndSearchGenericLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
